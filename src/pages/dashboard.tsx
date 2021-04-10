4d2179fe7041891a1755e56ed6a9d2af54574442
Mário César Silva <mcgato33@hotmail.com>
2021-04-10 18:43:14 -0100

    Componente: Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,21 @@
+import { Flex } from '@chakra-ui/react';
 import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
 
 export default function Dashboard() {
   return (
-    <>
+    <Flex direction="column" height="100vh">
       <Header />
-    </>
+
+      <Flex 
+        width="100%" 
+        maxWidth={1480} 
+        marginY="6" 
+        marginX="auto"  
+        paddingX="6"
+      >
+        <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
