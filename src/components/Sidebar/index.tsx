e014801c58dead1a0c9fca924fa4ad32d8855a78
Mário César Silva <mcgato33@hotmail.com>
2021-04-13 14:09:25 -0100

    Update index.tsx

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
+import { Box, Stack } from "@chakra-ui/react";
 import { RiDashboardLine, RiContactsLine, RiInputMethodLine, RiGitMergeLine } from "react-icons/ri";
 import { NavLink } from "./NavLink";
 import { NavSection } from "./NavSection";
