dffcdb669d5069460661c9101ef4e416b8ec0f75
Mário César Silva <mcgato33@hotmail.com>
2021-04-14 06:03:20 -0100

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,18 +1,21 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
 interface ProfileProps {
+  showProfileData?: boolean;
   name: string;
   email: string;
   avatarURL?: string;
 }
 
-export function Profile({ name, email, avatarURL }: ProfileProps) {
+export function Profile({ name, email, avatarURL, showProfileData = true }: ProfileProps) {
   return(
     <Flex alignItems="center">
-      <Box marginRight="4" textAlign="right">
-        <Text>{name}</Text>
-        <Text color="gray.300">{email}</Text>
-      </Box>
+      { showProfileData && (
+        <Box marginRight="4" textAlign="right">
+          <Text>{name}</Text>
+          <Text color="gray.300">{email}</Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Mário César" src={avatarURL} />
     </Flex>
